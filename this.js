da457289516d155e756d3f9f6714b33aac86420f
Andronik Pogosyan <apogosyan.exceedteam@gmail.com>
2020-10-21 15:20:49 +0300

    added this

---
+++ this.js
@@ -0,0 +1,36 @@
+// let calculator = {
+//     read() {
+//         this.firstNumber = +prompt('number 1', 10);
+//         this.secondNumber = +prompt('number 2', 20);
+//     },
+//     sum() {
+//        return this.firstNumber + this.secondNumber
+//     },
+//     mul() {
+//        return this.firstNumber * this.secondNumber
+//     }
+// }
+
+// calculator.read()
+
+
+// console.log(calculator.sum(), calculator.mul());
+
+// let ladder = {
+//     step: 0,
+//     up() {
+//         this.step++;
+//         return this
+//     },
+//     down() {
+//         this.step--;
+//         return this
+//     },
+//     showStep: function() {
+//         alert(this.step);
+//         return this
+//     }
+// };
+
+
+// console.log(ladder.up().up().down().showStep());
