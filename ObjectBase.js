8acca024f83042b74ce885af983537b0f430b783
Andronik Pogosyan <apogosyan.exceedteam@gmail.com>
2020-10-21 12:58:50 +0300

    added ObjectBase

---
+++ ObjectBase.js
@@ -0,0 +1,67 @@
+//задача 1
+
+// let user = {};
+
+// user.name = "John";
+
+// user.surname = "Smith";
+
+// user.name = "Pete";
+
+// delete user.name;
+// console.log(user)
+
+//Задача 2
+
+// let object = {
+
+// };
+
+// let isEmpty = (obj) => {
+//     for (let key in obj) {
+//         return false
+//     }
+//     return true
+
+
+// }
+
+// console.log(isEmpty(object))
+
+//Задача 4
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// let sum = (obj) => {
+//     let result = 0;
+//     for (let key in obj) {
+//         result += obj[key];
+//     }
+//     console.log(result)
+// }
+
+// sum(salaries);
+
+// Задача 5
+
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+
+// let multiplyNumeric = (obj) => {
+//     for (let key in obj) {
+//         if (typeof obj[key] === "number") {
+//             obj[key] *= 2;
+//         }
+//     }
+// }
+
+
+
+// multiplyNumeric(menu);
