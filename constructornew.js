8b219b9036330276772f352e5fd38cac23071cf8
Andronik Pogosyan <apogosyan.exceedteam@gmail.com>
2020-10-21 16:37:02 +0300

    added constructor

---
+++ constructornew.js
@@ -0,0 +1,38 @@
+//Задача 2
+
+// function Calc() {
+//     this.read = function() {
+//             this.first = +prompt('number 1', 10);
+//             this.second = +prompt('number 2', 15)
+//         },
+//         this.sum = function() {
+//             return this.first + this.second;
+//         },
+//         this.mul = function() {
+//             return this.first * this.second;
+//         }
+// }
+
+// let calculator = new Calc();
+
+// calculator.read();
+
+// alert("Sum=" + calculator.sum());
+// alert("Mul=" + calculator.mul());
+
+//задача 3
+
+
+// function Accumulator(v) {
+//     this.value = +v,
+//         this.read = function() {
+//             this.value += +prompt('num', 10);
+//         }
+// }
+
+// let acc = new Accumulator(5);
+
+// acc.read();
+// acc.read();
+
+// console.log(acc.value);
